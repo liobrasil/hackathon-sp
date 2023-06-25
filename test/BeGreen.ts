db0c2632e11af91681b471e4b4c2e62c8ec5e912
Lionel LIMOL <limol.lionel@gmail.com>
2023-06-25 01:31:07 -0300

    flatten contracts

--- test/BeGreen.ts
+++ test/BeGreen.ts
@@ -1,4 +1,4 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
